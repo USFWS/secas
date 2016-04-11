c50d7c17192010b5a557c3aa9be59e44c99e54c2
Roy Hewitt <roy_hewitt@fws.gov>
2016-04-11 15:20:18 -0400

    Fix baseurl issues

--- _src/js/homepage/hero.js
+++ _src/js/homepage/hero.js
@@ -5,37 +5,37 @@
 
   var images = [
     {
-      src: '../images/hero/black-rock-mountain-state-park-in-georgia-matthew-cimitile.jpg',
+      src: './images/hero/black-rock-mountain-state-park-in-georgia-matthew-cimitile.jpg',
       headerClass: 'dark-text',
       caption: 'Black Rock Mountain State Park, Georgia. Photo: Matthew Cimitile'
     },
     {
-      src: '../images/hero/big-lake-national-wildlife-refuge-bottomland-hardwoods.jpg',
+      src: './images/hero/big-lake-national-wildlife-refuge-bottomland-hardwoods.jpg',
       headerClass: 'light-text',
       caption: 'Big Lake National Wildlife Refuge. Photo: USFWS'
     },
     {
-      src: '../images/hero/canoeing-alligator-river-nationa-wildlife-refuge.jpg',
+      src: './images/hero/canoeing-alligator-river-nationa-wildlife-refuge.jpg',
       headerClass: 'light-text',
       caption: 'Alligator River National Wildlife Refuge. Photo: Steve Hillerbrand, USFWS'
     },
     {
-      src: '../images/hero/cascades-falls.jpg',
+      src: './images/hero/cascades-falls.jpg',
       headerClass: 'light-text',
       caption: 'Cascades falls, Virginia. Photo: Matthew Cimitile'
     },
     {
-      src: '../images/hero/gulf-side-lower-suwanee-national-wildlife-refuge.jpg',
+      src: './images/hero/gulf-side-lower-suwanee-national-wildlife-refuge.jpg',
       headerClass: 'light-text',
       caption: 'Lower Suwanee National Wildlife Refuge. Photo: George Willson'
     },
     {
-      src: '../images/hero/salt-marsh-st-simons-island.jpg',
+      src: './images/hero/salt-marsh-st-simons-island.jpg',
       headerClass: 'light-text',
       caption: 'Salt marsh at St. Simons Island, Georgia. Photo: USFWS'
     },
     {
-      src: '../images/hero/slash-pine-flatwoods-saw-palmetto-osceola-national-forest.jpg',
+      src: './images/hero/slash-pine-flatwoods-saw-palmetto-osceola-national-forest.jpg',
       headerClass: 'light-text',
       caption: 'Slash pine flatwoods at Osceola National Forest. <a href="https://flic.kr/p/9LmmDb">Photo</a>: <a href="https://www.flickr.com/photos/79666107@N00/">Chris M Morris</a>, <a href="https://creativecommons.org/licenses/by/2.0/">CC BY 2.0</a>'
     }
--- _src/js/homepage/nav.js
+++ _src/js/homepage/nav.js
@@ -39,7 +39,7 @@
   }
 
   function downloadIndex() {
-    xhr.get('/search.json', function (err, res) {
+    xhr.get('./search.json', function (err, res) {
       if (err) console.error('Could not download search index. :( ');
       options.data = JSON.parse(res.body);
       createIndex();
