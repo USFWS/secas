b9247f9fa7a68a66a94b280cd94be9c97e0ac19f
Roy Hewitt <roy_hewitt@fws.gov>
2016-04-14 07:40:04 -0400

    Moved images array to it's own file

---
+++ _src/js/homepage/images.js
@@ -0,0 +1,51 @@
+(function () {
+  'use strict';
+
+  module.exports = [
+    {
+      src: './images/hero/black-rock-mountain-state-park-in-georgia-matthew-cimitile.jpg',
+      headerClass: 'dark-text',
+      caption: '<a href="http://gastateparks.org/BlackRockMountain">Black Rock Mountain State Park</a>, Georgia. Photo: Matthew Cimitile'
+    },
+    {
+      src: './images/hero/big-lake-national-wildlife-refuge-bottomland-hardwoods.jpg',
+      headerClass: 'light-text',
+      caption: '<a href="http://www.fws.gov/biglake/">Big Lake National Wildlife Refuge</a>. Photo: USFWS'
+    },
+    {
+      src: './images/hero/canoeing-alligator-river-nationa-wildlife-refuge.jpg',
+      headerClass: 'light-text',
+      caption: '<a href="http://www.fws.gov/refuge/alligator_river/">Alligator River National Wildlife Refuge</a>. Photo: Steve Hillerbrand, USFWS'
+    },
+    {
+      src: './images/hero/cascades-falls.jpg',
+      headerClass: 'light-text',
+      caption: '<a href="http://gilescounty.org/cascades.html">Cascades falls</a>, Virginia. Photo: Matthew Cimitile'
+    },
+    {
+      src: './images/hero/gulf-side-lower-suwanee-national-wildlife-refuge.jpg',
+      headerClass: 'light-text',
+      caption: '<a href="http://www.fws.gov/refuge/lower_suwannee/">Lower Suwanee National Wildlife Refuge</a>. Photo: George Willson'
+    },
+    {
+      src: './images/hero/salt-marsh-st-simons-island.jpg',
+      headerClass: 'light-text',
+      caption: 'Salt marsh at St. Simons Island, Georgia. Photo: USFWS'
+    },
+    {
+      src: './images/hero/slash-pine-flatwoods-saw-palmetto-osceola-national-forest.jpg',
+      headerClass: 'light-text',
+      caption: 'Slash pine flatwoods at <a href="http://www.fs.usda.gov/osceola">Osceola National Forest</a>. <a href="https://flic.kr/p/9LmmDb">Photo</a>: <a href="https://www.flickr.com/photos/79666107@N00/">Chris M Morris</a>, <a href="https://creativecommons.org/licenses/by/2.0/">CC BY 2.0</a>'
+    },
+    {
+      src: './images/hero/decoys-delta-national-forest.jpg',
+      headerClass: 'light-text',
+      caption: 'Setting decoys in Delta National Forest.  Photo: Blair Tirpack, USGS'
+    },
+    {
+      src: './images/hero/flooded-boardwalk-white-river-nwr.jpg',
+      headerClass: 'light-text',
+      caption: 'Flooded boardwalk at <a href="http://www.fws.gov/refuge/white_river/">White River National Wildlife Refuge</a>. Photo: USFWS'
+    }
+  ];
+})();
