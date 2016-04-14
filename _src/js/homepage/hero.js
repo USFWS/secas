dc560dc3f16ba0a77bcb86387a29f6165f2a5f88
Roy Hewitt <roy_hewitt@fws.gov>
2016-04-14 07:33:28 -0400

    Add more links to hero image captions

--- _src/js/homepage/hero.js
+++ _src/js/homepage/hero.js
@@ -7,27 +7,27 @@
     {
       src: './images/hero/black-rock-mountain-state-park-in-georgia-matthew-cimitile.jpg',
       headerClass: 'dark-text',
-      caption: 'Black Rock Mountain State Park, Georgia. Photo: Matthew Cimitile'
+      caption: '<a href="http://gastateparks.org/BlackRockMountain">Black Rock Mountain State Park</a>, Georgia. Photo: Matthew Cimitile'
     },
     {
       src: './images/hero/big-lake-national-wildlife-refuge-bottomland-hardwoods.jpg',
       headerClass: 'light-text',
-      caption: 'Big Lake National Wildlife Refuge. Photo: USFWS'
+      caption: '<a href="http://www.fws.gov/biglake/">Big Lake National Wildlife Refuge</a>. Photo: USFWS'
     },
     {
       src: './images/hero/canoeing-alligator-river-nationa-wildlife-refuge.jpg',
       headerClass: 'light-text',
-      caption: 'Alligator River National Wildlife Refuge. Photo: Steve Hillerbrand, USFWS'
+      caption: '<a href="http://www.fws.gov/refuge/alligator_river/">Alligator River National Wildlife Refuge</a>. Photo: Steve Hillerbrand, USFWS'
     },
     {
       src: './images/hero/cascades-falls.jpg',
       headerClass: 'light-text',
-      caption: 'Cascades falls, Virginia. Photo: Matthew Cimitile'
+      caption: '<a href="http://gilescounty.org/cascades.html">Cascades falls</a>, Virginia. Photo: Matthew Cimitile'
     },
     {
       src: './images/hero/gulf-side-lower-suwanee-national-wildlife-refuge.jpg',
       headerClass: 'light-text',
-      caption: 'Lower Suwanee National Wildlife Refuge. Photo: George Willson'
+      caption: '<a href="http://www.fws.gov/refuge/lower_suwannee/">Lower Suwanee National Wildlife Refuge</a>. Photo: George Willson'
     },
     {
       src: './images/hero/salt-marsh-st-simons-island.jpg',
@@ -37,7 +37,7 @@
     {
       src: './images/hero/slash-pine-flatwoods-saw-palmetto-osceola-national-forest.jpg',
       headerClass: 'light-text',
-      caption: 'Slash pine flatwoods at Osceola National Forest. <a href="https://flic.kr/p/9LmmDb">Photo</a>: <a href="https://www.flickr.com/photos/79666107@N00/">Chris M Morris</a>, <a href="https://creativecommons.org/licenses/by/2.0/">CC BY 2.0</a>'
+      caption: 'Slash pine flatwoods at <a href="http://www.fs.usda.gov/osceola">Osceola National Forest</a>. <a href="https://flic.kr/p/9LmmDb">Photo</a>: <a href="https://www.flickr.com/photos/79666107@N00/">Chris M Morris</a>, <a href="https://creativecommons.org/licenses/by/2.0/">CC BY 2.0</a>'
     },
     {
       src: './images/hero/decoys-delta-national-forest.jpg',
