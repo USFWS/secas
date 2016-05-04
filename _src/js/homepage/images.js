6b2707e21ad1a9c6387362676e5b5f9d81cc0580
Roy Hewitt <roy_hewitt@fws.gov>
2016-05-04 09:54:46 -0400

    Add another hero image, make sure links in photo credits open in a new window

--- _src/js/homepage/images.js
+++ _src/js/homepage/images.js
@@ -5,27 +5,27 @@
     {
       src: './images/hero/black-rock-mountain-state-park-in-georgia-matthew-cimitile.jpg',
       headerClass: 'dark-text',
-      caption: '<a href="http://gastateparks.org/BlackRockMountain">Black Rock Mountain State Park</a>, Georgia. Photo: Matthew Cimitile'
+      caption: '<a href="http://gastateparks.org/BlackRockMountain" target="_blank">Black Rock Mountain State Park</a>, Georgia. Photo: Matthew Cimitile'
     },
     {
       src: './images/hero/big-lake-national-wildlife-refuge-bottomland-hardwoods.jpg',
       headerClass: 'light-text',
-      caption: '<a href="http://www.fws.gov/biglake/">Big Lake National Wildlife Refuge</a>. Photo: USFWS'
+      caption: '<a href="http://www.fws.gov/biglake/" target="_blank">Big Lake National Wildlife Refuge</a>. Photo: USFWS'
     },
     {
       src: './images/hero/canoeing-alligator-river-nationa-wildlife-refuge.jpg',
       headerClass: 'light-text',
-      caption: '<a href="http://www.fws.gov/refuge/alligator_river/">Alligator River National Wildlife Refuge</a>. Photo: Steve Hillerbrand, USFWS'
+      caption: '<a href="http://www.fws.gov/refuge/alligator_river/" target="_blank">Alligator River National Wildlife Refuge</a>. Photo: Steve Hillerbrand, USFWS'
     },
     {
       src: './images/hero/cascades-falls.jpg',
       headerClass: 'light-text',
-      caption: '<a href="http://gilescounty.org/cascades.html">Cascades falls</a>, Virginia. Photo: Matthew Cimitile'
+      caption: '<a href="http://gilescounty.org/cascades.html" target="_blank">Cascades falls</a>, Virginia. Photo: Matthew Cimitile'
     },
     {
       src: './images/hero/gulf-side-lower-suwanee-national-wildlife-refuge.jpg',
       headerClass: 'light-text',
-      caption: '<a href="http://www.fws.gov/refuge/lower_suwannee/">Lower Suwanee National Wildlife Refuge</a>. Photo: George Willson'
+      caption: '<a href="http://www.fws.gov/refuge/lower_suwannee/" target="_blank">Lower Suwanee National Wildlife Refuge</a>. Photo: George Willson'
     },
     {
       src: './images/hero/salt-marsh-st-simons-island.jpg',
@@ -35,7 +35,7 @@
     {
       src: './images/hero/slash-pine-flatwoods-saw-palmetto-osceola-national-forest.jpg',
       headerClass: 'light-text',
-      caption: 'Slash pine flatwoods at <a href="http://www.fs.usda.gov/osceola">Osceola National Forest</a>. <a href="https://flic.kr/p/9LmmDb">Photo</a>: <a href="https://www.flickr.com/photos/79666107@N00/">Chris M Morris</a>, <a href="https://creativecommons.org/licenses/by/2.0/">CC BY 2.0</a>'
+      caption: 'Slash pine flatwoods at <a href="http://www.fs.usda.gov/osceola" target="_blank">Osceola National Forest</a>. <a href="https://flic.kr/p/9LmmDb" target="_blank">Photo</a>: <a href="https://www.flickr.com/photos/79666107@N00/" target="_blank">Chris M Morris</a>, <a href="https://creativecommons.org/licenses/by/2.0/" target="_blank">CC BY 2.0</a>'
     },
     {
       src: './images/hero/decoys-delta-national-forest.jpg',
@@ -45,17 +45,22 @@
     {
       src: './images/hero/flooded-boardwalk-white-river-nwr.jpg',
       headerClass: 'light-text',
-      caption: 'Flooded boardwalk at <a href="http://www.fws.gov/refuge/white_river/">White River National Wildlife Refuge</a>. Photo: USFWS'
+      caption: 'Flooded boardwalk at <a href="http://www.fws.gov/refuge/white_river/" target="_blank">White River National Wildlife Refuge</a>. Photo: USFWS'
     },
     {
       src: './images/hero/okefenokee-water-lillies.jpg',
       headerClass: 'dark-text',
-      caption: 'Water Lilies, <a href="http://www.fws.gov/refuge/okefenokee/">Okefenokee National Wildlife Refuge</a>, Gerogia. <a href="https://flic.kr/p/5YdLp4">Photo</a>: <a href="https://www.flickr.com/photos/fkehren/">Frank Kehren</a> <a https://creativecommons.org/licenses/by-nc-nd/2.0/legalcode">CC-BY-NC-ND 2.0</a>'
+      caption: 'Water Lilies, <a href="http://www.fws.gov/refuge/okefenokee/" target="_blank">Okefenokee National Wildlife Refuge</a>, Gerogia. <a href="https://flic.kr/p/5YdLp4" target="_blank">Photo</a>: <a href="https://www.flickr.com/photos/fkehren/" target="_blank">Frank Kehren</a> <a https://creativecommons.org/licenses/by-nc-nd/2.0/legalcode" target="_blank">CC-BY-NC-ND 2.0</a>'
     },
     {
       src: './images/hero/sea-oats-dunes-cape-hatteras-national-seashore.jpg',
       headerClass: 'light-text',
-      caption: 'Sea oats and dunes at <a href="https://www.nps.gov/caha">Cape Hatteras National Seashore</a>, North Carolina. Photo: National Park Service'
+      caption: 'Sea oats and dunes at <a href="https://www.nps.gov/caha" target="_blank">Cape Hatteras National Seashore</a>, North Carolina. Photo: National Park Service'
+    },
+    {
+      src: './images/hero/grays-reef-national-marine-sanctuary.jpg',
+      headerClass: 'light-text',
+      caption: 'Diver, sponges, corals and fishes at <a href="http://graysreef.noaa.gov/" target="_blank">Gray\'s Reef National Marine Sanctuary</a>, Savannah, Georgia. <a href="https://flic.kr/p/9RoLGE" target="_blank">Photo</a>: Greg McFall, NOAA'
     }
   ];
 })();
