a17c5a386776c3ac8bc37f73619ca7ed5b46c042
Roy Hewitt <roy_hewitt@fws.gov>
2016-12-01 08:13:51 -0500

    Install lodash.filter

--- _src/js/homepage/util.js
+++ _src/js/homepage/util.js
@@ -5,6 +5,7 @@
     defaults: require('lodash.defaults'),
     random: require('lodash.sample'),
     each: require('lodash.forEach'),
+    filter: require('lodash.filter'),
     addClass: require('add-class'),
     removeClass: require('remove-class')
   };
