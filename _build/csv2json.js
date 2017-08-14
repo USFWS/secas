2d1df5cacd4bc791ecf2b668acc5524fdb628421
Roy Hewitt <roy_hewitt@fws.gov>
2017-08-14 09:36:51 -0400

    Add npm script to convert CSV to JSON

--- _build/csv2json.js
+++ _build/csv2json.js
@@ -4,11 +4,7 @@ const Parser = require('babyparse');
 const input = './data/bloggable.csv';
 const output = input.replace('csv', 'js');
 
-const options = {
-  header: true
-}
-
-const parsed = Parser.parseFiles(input, options);
+const parsed = Parser.parseFiles(input, { header: true });
 
 fs.writeFile(output, JSON.stringify(parsed.data), 'utf8', (err) => {
   if (err) throw err;
