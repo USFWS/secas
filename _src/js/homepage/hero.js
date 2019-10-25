672394d9a9e494883a767f366aec1637f2038477
Roy Hewitt <roy_hewitt@fws.gov>
2019-10-25 10:31:11 -0400

    Move blog images back into blog body

--- _src/js/homepage/hero.js
+++ _src/js/homepage/hero.js
@@ -7,11 +7,6 @@ function init() {
   const caption = document.querySelector('.hero-image-caption');
   const header = document.querySelector('.main-header');
   const hero = document.querySelector('.hero-image');
-
-  // Use image from post if it exists
-  if (hero.style.backgroundImage !== 'url("/images/")') return;
-
-  // Otherwise use a random image
   const img = _.random(images);
   caption.innerHTML = img.caption;
   hero.setAttribute('style', `background-image: url(${baseurl + img.src});`);
