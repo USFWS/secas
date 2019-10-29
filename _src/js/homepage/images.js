2988b8c3271814ebd2b274ca7adbea66cf6b8b95
Roy Hewitt <roy_hewitt@fws.gov>
2019-10-29 09:22:51 -0400

    Update hero images

--- _src/js/homepage/images.js
+++ _src/js/homepage/images.js
@@ -2,10 +2,10 @@
 module.exports = [
   {
     src:
-      '/images/hero/black-rock-mountain-state-park-in-georgia-matthew-cimitile.jpg',
+      '/images/hero/great-smokies-mountain-skyline.jpg',
     headerClass: 'dark-text',
     caption:
-      '<a href="http://gastateparks.org/BlackRockMountain" target="_blank">Black Rock Mountain State Park</a>, Georgia. Photo by Matthew Cimitile.',
+      '<a href="https://flic.kr/p/RfZUfE" target="_blank">Great Smoky Mountains National Park.</a>, Georgia. Photo by <a href="https://www.flickr.com/photos/25171569@N02/">jjjj56cp</a>, <a href="https://creativecommons.org/licenses/by-nc-sa/2.0/">CC BY-NC-SA 2.0</a>.',
   },
   {
     src:
@@ -21,10 +21,10 @@ module.exports = [
       '<a href="http://www.fws.gov/refuge/alligator_river/" target="_blank">Alligator River National Wildlife Refuge</a>. Photo by Steve Hillerbrand, USFWS.',
   },
   {
-    src: '/images/hero/cascades-falls.jpg',
+    src: '/images/hero/big-laurel-branch-falls-nantahala-national-forest-north-carolina.jpg',
     headerClass: 'light-text',
     caption:
-      '<a href="http://gilescounty.org/cascades.html" target="_blank">Cascades falls</a>, Virginia. Photo by Matthew Cimitile.',
+      '<a href="https://flic.kr/p/r1Wk6B" target="_blank">Big Laurel Branch Falls, Nantahala National Forest</a>, North Carolina. Photo by Jeff Moore, <a href="https://creativecommons.org/licenses/by-nc/2.0/">CC BY-NC 2.0</a>.',
   },
   {
     src: '/images/hero/gulf-side-lower-suwanee-national-wildlife-refuge.jpg',
@@ -39,10 +39,10 @@ module.exports = [
   },
   {
     src:
-      '/images/hero/slash-pine-flatwoods-saw-palmetto-osceola-national-forest.jpg',
+      '/images/hero/longleaf-pine-at-carolina-sandhills-national-wildlife-refuge.jpg',
     headerClass: 'light-text',
     caption:
-      'Slash pine flatwoods at <a href="http://www.fs.usda.gov/osceola" target="_blank">Osceola National Forest</a>. <a href="https://flic.kr/p/9LmmDb" target="_blank">Photo</a> by <a href="https://www.flickr.com/photos/79666107@N00/" target="_blank">Chris M Morris</a>, <a href="https://creativecommons.org/licenses/by/2.0/" target="_blank">CC BY 2.0</a>.',
+      'Longleaf pine at <a href="https://www.fws.gov/refuge/Carolina_Sandhills/">Carolina Sandhills National Wildlife Refuge</a>, South Carolina. Photo by Jack Culpepper, USFWS.',
   },
   {
     src: '/images/hero/decoys-delta-national-forest.jpg',
@@ -51,27 +51,27 @@ module.exports = [
       'Setting decoys in Delta National Forest.  Photo by Blair Tirpak, USGS.',
   },
   {
-    src: '/images/hero/flooded-boardwalk-white-river-nwr.jpg',
+    src: '/images/hero/laguna-cartagena.jpg',
     headerClass: 'light-text',
     caption:
-      'Flooded boardwalk at <a href="http://www.fws.gov/refuge/white_river/" target="_blank">White River National Wildlife Refuge</a>. Photo by USFWS.',
+      '<a href="https://flic.kr/p/CemsUh" target="_blank">Laguna Cartagena National Wildlife Refuge</a>, Puerto Rico. Photo by M. Morel, UFSWS.',
   },
   {
-    src: '/images/hero/okefenokee-water-lillies.jpg',
+    src: '/images/hero/virginia-creeper-trail.jpg',
+    headerClass: 'light-text',
+    caption:
+      'Virginia Creeper Trail, Virginia. Photo by <a href="https://flickr.com/photos/27864969@N00/">Stephen Taylor</a>, <a href="https://creativecommons.org/licenses/by-nc/2.0/">CC BY-NC 2.0</a>',
+  },
+  {
+    src: '/images/hero/cape-lookout-lighthouse.jpg',
     headerClass: 'dark-text',
     caption:
-      'Water Lilies, <a href="http://www.fws.gov/refuge/okefenokee/" target="_blank">Okefenokee National Wildlife Refuge</a>, Georgia. <a href="https://flic.kr/p/5YdLp4" target="_blank">Photo</a> by <a href="https://www.flickr.com/photos/fkehren/" target="_blank">Frank Kehren</a> <a https://creativecommons.org/licenses/by-nc-nd/2.0/legalcode" target="_blank">CC-BY-NC-ND 2.0</a>.',
+      '<a href="https://flic.kr/p/aiCPu1" target="_blank">Cape Lookout lighthouse and sea oats</a>, North Carolina. Photo by Zach Frailey, <a href="https://creativecommons.org/licenses/by-nc-nd/2.0/">CC BY-NC-ND 2.0</a>.',
   },
   {
-    src: '/images/hero/sea-oats-dunes-cape-hatteras-national-seashore.jpg',
+    src: '/images/hero/grays-reef.jpg',
     headerClass: 'light-text',
     caption:
-      'Sea oats and dunes at <a href="https://www.nps.gov/caha" target="_blank">Cape Hatteras National Seashore</a>, North Carolina. Photo by The National Park Service.',
-  },
-  {
-    src: '/images/hero/grays-reef-national-marine-sanctuary.jpg',
-    headerClass: 'light-text',
-    caption:
-      'Diver, sponges, corals and fishes at <a href="http://graysreef.noaa.gov/" target="_blank">Gray\'s Reef National Marine Sanctuary</a>, Savannah, Georgia. <a href="https://flic.kr/p/9RoLGE" target="_blank">Photo</a> by Greg McFall, NOAA.',
+      'Diving at Gray’s Reef National Marine Sanctuary, Savannah, Georgia. Photo by Greg McFall, NOAA.',
   },
 ];
