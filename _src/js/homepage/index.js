f672f55bde6c754e1dd84fa9e5bc8b23180f4093
Roy Hewitt <roy_hewitt@fws.gov>
2020-07-15 13:41:54 -0400

    Remove custom subscriber form

--- _src/js/homepage/index.js
+++ _src/js/homepage/index.js
@@ -2,13 +2,6 @@ const Parallax = require('parallax-scroll');
 
 const nav = require('./nav');
 const hero = require('./hero');
-const granicus = require('./granicus');
-
-const form = document.querySelector('.subscriber-form');
-const fieldset = form.querySelector('fieldset');
-const email = document.querySelector('#email');
-const error = document.querySelector('.error-message');
-const success = document.querySelector('.form-success');
 
 const parallax = new Parallax('.parallax', { speed: 0.4 });
 parallax.animate();
@@ -20,23 +13,4 @@ nav.init({
   form: document.querySelector('.search-form'),
   searchInput: document.getElementById('search-input'),
   results: document.querySelector('.search-results'),
-});
-
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
-  fieldset.disabled = true;
-
-  if (granicus.validate(email.value)) {
-    granicus.subscribe(email.value)
-      .then((res) => {
-        fieldset.disabled = false;
-        fieldset.classList.add('complete');
-        success.setAttribute('aria-hidden', false);
-      })
-      .catch((e) => {
-        granicus.displayMessage('An error occurred. Try again later.');
-      });
-  } else {
-    granicus.displayMessage('You must enter a valid email address', error);
-  }
 });
