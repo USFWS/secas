049a12696362e6b830ebc18955593c69d0a30bb2
Brendan C. Ward <bcward@astutespruce.com>
2026-05-07 10:56:24 -0700

    Add core svelte config files

---
+++ svelte.config.js
@@ -0,0 +1,38 @@
+import { mdsvex } from 'mdsvex';
+import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	compilerOptions: {
+		// Force runes mode for the project, except for libraries. Can be removed in svelte 6.
+		runes: ({ filename }) => (filename.split(/[/\\]/).includes('node_modules') ? undefined : true)
+	},
+	kit: {
+		adapter: adapter({
+			pages: 'public',
+			assets: 'public',
+			fallback: 'index.html',
+			precompress: false,
+			strict: true
+		}),
+		paths: {
+			// set proper path on github pages if deploying to path
+			base: process.env.DEPLOY_PATH || ''
+		},
+		alias: {
+			$content: 'content'
+		}
+	},
+	preprocess: [
+		vitePreprocess(),
+		mdsvex({
+			extensions: ['.md'],
+			remarkPlugins: [enhancedImages],
+			rehypePlugins: [[rehypeExternalLinks, { target: '_blank' }]]
+		})
+	],
+	extensions: ['.svelte', '.md']
+};
+
+export default config;
---
+++ vite.config.ts
@@ -0,0 +1,6 @@
+import tailwindcss from '@tailwindcss/vite';
+import { sveltekit } from '@sveltejs/kit/vite';
+import { enhancedImages } from '@sveltejs/enhanced-img';
+import { defineConfig } from 'vite';
+
+export default defineConfig({ plugins: [tailwindcss(), enhancedImages(), sveltekit()] });
