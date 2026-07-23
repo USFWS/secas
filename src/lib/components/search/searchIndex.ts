e951b8094b9c6eb8a9e3cb02ff2d937afc0884fe
Brendan C. Ward <bcward@astutespruce.com>
2026-07-23 13:29:22 -0700

    Improve search

--- src/lib/components/search/searchIndex.ts
+++ src/lib/components/search/searchIndex.ts
@@ -17,12 +17,16 @@ export const loadIndex = async () => {
 	const searchItems = (await response.json()) as SearchItem[]
 	const index = new Fuse<SearchItem>(searchItems, {
 		keys: [
-			{ name: 'title', weight: 10 },
-			{ name: 'description', weight: 3 },
+			{ name: 'title', weight: 100 },
+			{ name: 'description', weight: 1 },
 			{ name: 'author', weight: 1 }
 		],
+		includeScore: true,
+		minMatchCharLength: 3,
 		threshold: 0.25,
-		includeScore: true
+		useTokenSearch: true,
+		tokenMatch: 'all',
+		ignoreLocation: true
 	})
 
 	return index
